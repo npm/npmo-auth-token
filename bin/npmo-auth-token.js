2fc6e7a80ad7df25287503e9c22be59270f2d063
Benjamin Coe <ben@npmjs.com>
2016-01-25 16:13:22 -0800

    managed to get my fix landed in inquirer.js super fast

--- bin/npmo-auth-token.js
+++ bin/npmo-auth-token.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var chalk = require('chalk')
-var inquirer = require('@bcoe/inquirer')
+var inquirer = require('inquirer')
 var Session = require('../').Session
 
 function generateToken (userInfo) {
